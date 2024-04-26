8b380b79c2f8bf12b771912efe23708c3990db2e
Arduinum <message.chaos628@gmail.com>
2024-04-26 21:53:14 +0300

    feat: added models for quiz

--- quiz_app/admin.py
+++ quiz_app/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from quiz_app.models import (
+    TypePublicCatering,
+    TypeRetail,
+    AppointmentService,
+    Quiz
+)
 
-# Register your models here.
+
+class QuizAdmin(admin.ModelAdmin):
+    """
+    Класс для настройки отображения полей модели
+    """
+
+    readonly_fields = ('created_at', )
+
+
+admin.site.register(TypePublicCatering)
+admin.site.register(TypeRetail)
+admin.site.register(AppointmentService)
+admin.site.register(Quiz, QuizAdmin)
