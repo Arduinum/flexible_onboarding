8faefaa8bfe32c858016cf8924ee4a1e17172902
Arduinum <message.chaos628@gmail.com>
2024-05-17 12:08:07 +0300

    docs: comment for test gitea

--- flelexible_onbording/settings.py
+++ flelexible_onbording/settings.py
@@ -139,3 +139,5 @@ STATICFILES_DIRS = [STATIC_DIR]
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# comment for test gitea
